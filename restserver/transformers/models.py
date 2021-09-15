51f24b9563e84c8ee862d3fdb3d57b6687ce0e68
= <gaingaius123@gmail.com>
2021-09-15 14:24:51 +0530

    defined model

--- restserver/transformers/models.py
+++ restserver/transformers/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Transformer(models.model):
+class Transformer(models.Model):
     name = models.CharField(max_length=150,unique=True)
     alternate_mode = models.CharField(max_length=250,blank=True,null=True)
     description = models.CharField(max_length=500,blank=True,null=True)
     alive = models.BooleanField(default=False)
     
     class Meta:
-        ordering = ('-id')
+        ordering = ('-id',)
 
     def __str__(self):
         return self.name
