2531a7865383071a0c20613b96334f1340307c6a
= <gaingaius123@gmail.com>
2021-09-23 09:50:41 +0530

    admin updated

--- library/catalog/admin.py
+++ library/catalog/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
 from .models import Book,Author,Genre,BookInstance,Language
+
+from django.views import generic
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+#admin.site.register(Author)
+
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status','borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back','borrower')
+        }),
+        
+    )
+
 admin.site.register(Language)
+
